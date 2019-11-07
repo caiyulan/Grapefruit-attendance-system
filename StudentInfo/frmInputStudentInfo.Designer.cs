18a2b635fd4058d372a4949ba4512e8c94601ec6
jiao54 <2676087477@qq.com>
2019-11-07 13:37:52 +0800

    StudentInfo

---
+++ StudentInfo/frmInputStudentInfo.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace StudentAttendanceMgr.StudentInfo
+{
+    partial class frmInputStudentInfo
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmInputStudentInfo));
+            this.btnInput = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.cboDormName = new System.Windows.Forms.ComboBox();
+            this.cboClassName = new System.Windows.Forms.ComboBox();
+            this.tsslblDesc = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cboSex = new System.Windows.Forms.ComboBox();
+            this.lblHomePhone = new System.Windows.Forms.Label();
+            this.lblHomeAddress = new System.Windows.Forms.Label();
+            this.lblTelephone = new System.Windows.Forms.Label();
+            this.lblDormName = new System.Windows.Forms.Label();
+            this.lblClassName = new System.Windows.Forms.Label();
+            this.lblSex = new System.Windows.Forms.Label();
+            this.lblStuName = new System.Windows.Forms.Label();
+            this.lblStuId = new System.Windows.Forms.Label();
+            this.statusStripDesc = new System.Windows.Forms.StatusStrip();
+            this.txtStuId = new System.Windows.Forms.TextBox();
+            this.txtStuName = new System.Windows.Forms.TextBox();
+            this.txtTelephone = new System.Windows.Forms.TextBox();
+            this.txtHomeAddress = new System.Windows.Forms.TextBox();
+            this.txtHomePhone = new System.Windows.Forms.TextBox();
+            this.statusStripDesc.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(61, 418);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(55, 23);
+            this.btnInput.TabIndex = 47;
+            this.btnInput.Text = "录入";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(280, 418);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(55, 23);
+            this.btnCancel.TabIndex = 46;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(207, 418);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(55, 23);
+            this.btnSave.TabIndex = 45;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(134, 418);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(55, 23);
+            this.btnNext.TabIndex = 44;
+            this.btnNext.Text = "下一条";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // cboDormName
+            // 
+            this.cboDormName.FormattingEnabled = true;
+            this.cboDormName.Location = new System.Drawing.Point(130, 227);
+            this.cboDormName.Name = "cboDormName";
+            this.cboDormName.Size = new System.Drawing.Size(205, 20);
+            this.cboDormName.TabIndex = 37;
+            // 
+            // cboClassName
+            // 
+            this.cboClassName.FormattingEnabled = true;
+            this.cboClassName.Location = new System.Drawing.Point(130, 181);
+            this.cboClassName.Name = "cboClassName";
+            this.cboClassName.Size = new System.Drawing.Size(205, 20);
+            this.cboClassName.TabIndex = 35;
+            // 
+            // tsslblDesc
+            // 
+            this.tsslblDesc.Name = "tsslblDesc";
+            this.tsslblDesc.Size = new System.Drawing.Size(329, 17);
+            this.tsslblDesc.Text = "说明：可连续输入多条记录，输入结束后，单击“下一条”。";
+            // 
+            // cboSex
+            // 
+            this.cboSex.FormattingEnabled = true;
+            this.cboSex.Items.AddRange(new object[] {
+            "男",
+            "女"});
+            this.cboSex.Location = new System.Drawing.Point(130, 135);
+            this.cboSex.Name = "cboSex";
+            this.cboSex.Size = new System.Drawing.Size(205, 20);
+            this.cboSex.TabIndex = 34;
+            this.cboSex.Text = "男";
+            // 
+            // lblHomePhone
+            // 
+            this.lblHomePhone.AutoSize = true;
+            this.lblHomePhone.Location = new System.Drawing.Point(59, 368);
+            this.lblHomePhone.Name = "lblHomePhone";
+            this.lblHomePhone.Size = new System.Drawing.Size(65, 12);
+            this.lblHomePhone.TabIndex = 32;
+            this.lblHomePhone.Text = "家庭电话：";
+            // 
+            // lblHomeAddress
+            // 
+            this.lblHomeAddress.AutoSize = true;
+            this.lblHomeAddress.Location = new System.Drawing.Point(59, 322);
+            this.lblHomeAddress.Name = "lblHomeAddress";
+            this.lblHomeAddress.Size = new System.Drawing.Size(65, 12);
+            this.lblHomeAddress.TabIndex = 31;
+            this.lblHomeAddress.Text = "家庭住址：";
+            // 
+            // lblTelephone
+            // 
+            this.lblTelephone.AutoSize = true;
+            this.lblTelephone.Location = new System.Drawing.Point(59, 276);
+            this.lblTelephone.Name = "lblTelephone";
+            this.lblTelephone.Size = new System.Drawing.Size(65, 12);
+            this.lblTelephone.TabIndex = 30;
+            this.lblTelephone.Text = "联系电话：";
+            // 
+            // lblDormName
+            // 
+            this.lblDormName.AutoSize = true;
+            this.lblDormName.Location = new System.Drawing.Point(59, 230);
+            this.lblDormName.Name = "lblDormName";
+            this.lblDormName.Size = new System.Drawing.Size(65, 12);
+            this.lblDormName.TabIndex = 29;
+            this.lblDormName.Text = "宿舍名称：";
+            // 
+            // lblClassName
+            // 
+            this.lblClassName.AutoSize = true;
+            this.lblClassName.Location = new System.Drawing.Point(59, 184);
+            this.lblClassName.Name = "lblClassName";
+            this.lblClassName.Size = new System.Drawing.Size(65, 12);
+            this.lblClassName.TabIndex = 28;
+            this.lblClassName.Text = "班级名称：";
+            // 
+            // lblSex
+            // 
+            this.lblSex.AutoSize = true;
+            this.lblSex.Location = new System.Drawing.Point(59, 138);
+            this.lblSex.Name = "lblSex";
+            this.lblSex.Size = new System.Drawing.Size(65, 12);
+            this.lblSex.TabIndex = 27;
+            this.lblSex.Text = "性    别：";
+            // 
+            // lblStuName
+            // 
+            this.lblStuName.AutoSize = true;
+            this.lblStuName.Location = new System.Drawing.Point(59, 92);
+            this.lblStuName.Name = "lblStuName";
+            this.lblStuName.Size = new System.Drawing.Size(65, 12);
+            this.lblStuName.TabIndex = 26;
+            this.lblStuName.Text = "姓    名：";
+            // 
+            // lblStuId
+            // 
+            this.lblStuId.AutoSize = true;
+            this.lblStuId.Location = new System.Drawing.Point(59, 46);
+            this.lblStuId.Name = "lblStuId";
+            this.lblStuId.Size = new System.Drawing.Size(65, 12);
+            this.lblStuId.TabIndex = 25;
+            this.lblStuId.Text = "学    号：";
+            // 
+            // statusStripDesc
+            // 
+            this.statusStripDesc.Enabled = false;
+            this.statusStripDesc.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsslblDesc});
+            this.statusStripDesc.Location = new System.Drawing.Point(0, 544);
+            this.statusStripDesc.Name = "statusStripDesc";
+            this.statusStripDesc.Size = new System.Drawing.Size(792, 22);
+            this.statusStripDesc.TabIndex = 24;
+            this.statusStripDesc.Text = "statusStrip1";
+            // 
+            // txtStuId
+            // 
+            this.txtStuId.Location = new System.Drawing.Point(130, 43);
+            this.txtStuId.Name = "txtStuId";
+            this.txtStuId.Size = new System.Drawing.Size(205, 21);
+            this.txtStuId.TabIndex = 48;
+            // 
+            // txtStuName
+            // 
+            this.txtStuName.Location = new System.Drawing.Point(130, 89);
+            this.txtStuName.Name = "txtStuName";
+            this.txtStuName.Size = new System.Drawing.Size(205, 21);
+            this.txtStuName.TabIndex = 49;
+            // 
+            // txtTelephone
+            // 
+            this.txtTelephone.Location = new System.Drawing.Point(130, 273);
+            this.txtTelephone.Name = "txtTelephone";
+            this.txtTelephone.Size = new System.Drawing.Size(205, 21);
+            this.txtTelephone.TabIndex = 50;
+            // 
+            // txtHomeAddress
+            // 
+            this.txtHomeAddress.Location = new System.Drawing.Point(130, 319);
+            this.txtHomeAddress.Name = "txtHomeAddress";
+            this.txtHomeAddress.Size = new System.Drawing.Size(205, 21);
+            this.txtHomeAddress.TabIndex = 51;
+            // 
+            // txtHomePhone
+            // 
+            this.txtHomePhone.Location = new System.Drawing.Point(130, 365);
+            this.txtHomePhone.Name = "txtHomePhone";
+            this.txtHomePhone.Size = new System.Drawing.Size(205, 21);
+            this.txtHomePhone.TabIndex = 52;
+            // 
+            // frmInputStudentInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 566);
+            this.Controls.Add(this.txtHomePhone);
+            this.Controls.Add(this.txtHomeAddress);
+            this.Controls.Add(this.txtTelephone);
+            this.Controls.Add(this.txtStuName);
+            this.Controls.Add(this.txtStuId);
+            this.Controls.Add(this.btnInput);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.cboDormName);
+            this.Controls.Add(this.cboClassName);
+            this.Controls.Add(this.cboSex);
+            this.Controls.Add(this.lblHomePhone);
+            this.Controls.Add(this.lblHomeAddress);
+            this.Controls.Add(this.lblTelephone);
+            this.Controls.Add(this.lblDormName);
+            this.Controls.Add(this.lblClassName);
+            this.Controls.Add(this.lblSex);
+            this.Controls.Add(this.lblStuName);
+            this.Controls.Add(this.lblStuId);
+            this.Controls.Add(this.statusStripDesc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmInputStudentInfo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "学生信息录入";
+            this.Load += new System.EventHandler(this.frmInputStudentInf_Load);
+            this.statusStripDesc.ResumeLayout(false);
+            this.statusStripDesc.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.ComboBox cboDormName;
+        private System.Windows.Forms.ComboBox cboClassName;
+        private System.Windows.Forms.ToolStripStatusLabel tsslblDesc;
+        private System.Windows.Forms.ComboBox cboSex;
+        private System.Windows.Forms.Label lblHomePhone;
+        private System.Windows.Forms.Label lblHomeAddress;
+        private System.Windows.Forms.Label lblTelephone;
+        private System.Windows.Forms.Label lblDormName;
+        private System.Windows.Forms.Label lblClassName;
+        private System.Windows.Forms.Label lblSex;
+        private System.Windows.Forms.Label lblStuName;
+        private System.Windows.Forms.Label lblStuId;
+        private System.Windows.Forms.StatusStrip statusStripDesc;
+        private System.Windows.Forms.TextBox txtStuId;
+        private System.Windows.Forms.TextBox txtStuName;
+        private System.Windows.Forms.TextBox txtTelephone;
+        private System.Windows.Forms.TextBox txtHomeAddress;
+        private System.Windows.Forms.TextBox txtHomePhone;
+    }
+}
---
+++ StudentInfo/frmInputStudentInfo.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using StudentAttendanceMgr.CommonClass;
+
+namespace StudentAttendanceMgr.StudentInfo
+{
+    public partial class frmInputStudentInfo : Form
+    {
+        // 定义存储数据的临时表
+        private DataTable dt;
+
+        public frmInputStudentInfo()
+        {
+            InitializeComponent();
+        }
+
+        // 填充班级名称组合框
+        private void FillClassNameComboBox()
+        {
+            try
+            {
+                //查询记录用的sql语句
+                string selectSql = "select ClassId, ClassName from Classes order by ClassId asc";
+
+                //创建数据集
+                DataSet dsClasses = new DataSet("Attendance");
+
+                //填充数据集
+                dsClasses = DBHelper.getDataSet(selectSql, "Classes");
+
+                //指定“班级名称组合框”的数据源
+                this.cboClassName.DataSource = dsClasses.Tables["Classes"];
+                this.cboClassName.DisplayMember = "ClassName";
+                this.cboClassName.ValueMember = "ClassId";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 填充宿舍名称组合框
+        private void FillDormNameComboBox()
+        {
+            try
+            {
+                //查询记录用的sql语句
+                string selectSql = "select DormId, DormName from Dorms order by DormId asc";
+
+                //创建数据集
+                DataSet dsClasses = new DataSet("Attendance");
+
+                //填充数据集
+                dsClasses = DBHelper.getDataSet(selectSql, "Dorms");
+
+                //指定“宿舍名称组合框”的数据源
+                this.cboDormName.DataSource = dsClasses.Tables["Dorms"];
+                this.cboDormName.DisplayMember = "DormName";
+                this.cboDormName.ValueMember = "DormId";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 清空文本框
+        private void ClearTextBox()
+        {
+            this.txtStuId.Clear();
+            this.txtStuName.Clear();
+            this.cboSex.Text = "";
+            this.cboClassName.Text = "";
+            this.cboDormName.Text = "";
+            this.txtTelephone.Clear();
+            this.txtHomeAddress.Clear();
+            this.txtHomePhone.Clear();
+        }
+
+        // 锁定文本框，不允许用户输入
+        private void LockedTextBox()
+        {
+            this.txtStuId.Enabled = false;
+            this.txtStuName.Enabled = false;
+            this.cboSex.Enabled = false;
+            this.cboClassName.Enabled = false;
+            this.cboDormName.Enabled = false;
+            this.txtTelephone.Enabled = false;
+            this.txtHomeAddress.Enabled = false;
+            this.txtHomePhone.Enabled = false;
+        }
+
+        // 解除锁定，允许用户输入
+        private void UnLockedTextBox()
+        {
+            this.txtStuId.Enabled = true;
+            this.txtStuName.Enabled = true;
+            this.cboSex.Enabled = true;
+            this.cboClassName.Enabled = true;
+            this.cboDormName.Enabled = true;
+            this.txtTelephone.Enabled = true;
+            this.txtHomeAddress.Enabled = true;
+            this.txtHomePhone.Enabled = true;
+        }
+
+        // 验证用户输入
+        private bool ValidateInput()
+        {
+            if (this.txtStuId.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入学号", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtStuId.Focus();
+                return false;
+            }
+            else if (this.txtStuName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入姓名", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtStuName.Focus();
+                return false;
+            }
+            else if (this.cboSex.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入性别", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.cboSex.Focus();
+                return false;
+            }
+            else if (this.cboClassName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入班级名称", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.cboClassName.Focus();
+                return false;
+            }
+            else if (this.cboDormName.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入宿舍名称", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.cboDormName.Focus();
+                return false;
+            }
+            else if (this.txtTelephone.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入联系电话", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtTelephone.Focus();
+                return false;
+            }
+            else if (this.txtHomeAddress.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入家庭住址", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtHomeAddress.Focus();
+                return false;
+            }
+            else if (this.txtHomePhone.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入家庭电话", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtHomePhone.Focus();
+                return false;
+            }
+
+            //检查后台数据库的 Students 表中是否存在重复的学号
+            string selectSql = String.Format("select count(*) from Students where StuId = '{0}'", this.txtStuId.Text);
+
+            //执行查询操作
+            int result = (int)DBHelper.executeScalar(selectSql);
+
+            //如果学号已存在，则提示用户重新输入，并终止操作。
+            if (result >= 1)
+            {
+                MessageBox.Show("该学号已存在，请重新输入！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtStuId.Focus();
+                return false;
+            }
+
+            //检查临时表中是否存在重复的学号
+            DataRow[] dataRowArray = null;
+
+            dataRowArray = dt.Select(String.Format("StuId = '{0}'", this.txtStuId.Text));
+
+            if (dataRowArray.Length >= 1)
+            {
+                MessageBox.Show("该学号已存在，请重新输入！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtStuId.Focus();
+                return false;
+            }
+
+            return true;
+        }
+
+        // 窗体加载
+        private void frmInputStudentInf_Load(object sender, EventArgs e)
+        {
+            // 初始化班级名称组合框
+            this.FillClassNameComboBox();
+
+            // 初始化宿舍名称组合框
+            this.FillDormNameComboBox();
+
+            try
+            {
+                // 创建存储数据的临时表
+                dt = new DataTable("Students");
+
+                // 添加列
+                dt.Columns.Add("StuId", typeof(string));
+                dt.Columns.Add("StuName", typeof(string));
+                dt.Columns.Add("Sex", typeof(string));
+                dt.Columns.Add("ClassId", typeof(string));
+                dt.Columns.Add("DormId", typeof(string));
+                dt.Columns.Add("Telephone", typeof(string));
+                dt.Columns.Add("HomeAddress", typeof(string));
+                dt.Columns.Add("HomePhone", typeof(string));
+
+                this.btnNext.Enabled = false;   //禁用“下一条”按钮
+                this.btnSave.Enabled = false;   //禁用“保存”按钮
+                this.btnCancel.Enabled = false; //禁用“取消”按钮
+
+                // 清空文本框
+                this.ClearTextBox();
+
+                // 锁定文本框
+                this.LockedTextBox();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 录入
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            // 清空临时表
+            dt.Rows.Clear();
+
+            //清空文本框
+            this.ClearTextBox();
+
+            //解除锁定
+            this.UnLockedTextBox();
+
+            this.btnInput.Enabled = false; //禁用“录入”按钮
+            this.btnNext.Enabled = true;   //启用“下一条”按钮
+            this.btnSave.Enabled = true;   //启用“保存”按钮
+            this.btnCancel.Enabled = true; //启用“取消”按钮
+
+            this.txtStuId.Focus();
+        }
+
+        // 下一条
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // 验证用户输入
+            if (this.ValidateInput() == false)
+            {
+                return;
+            }
+
+            try
+            {
+                // 添加新行
+                DataRow dr = dt.NewRow();
+                dr["StuId"] = this.txtStuId.Text;
+                dr["StuName"] = this.txtStuName.Text;
+                dr["Sex"] = this.cboSex.Text;
+                dr["ClassId"] = this.cboClassName.SelectedValue;
+                dr["DormId"] = this.cboDormName.SelectedValue;
+                dr["Telephone"] = this.txtTelephone.Text;
+                dr["HomeAddress"] = this.txtHomeAddress.Text;
+                dr["HomePhone"] = this.txtHomePhone.Text;
+
+                // 加入新行
+                dt.Rows.Add(dr);
+
+                //清空文本框
+                this.ClearTextBox();
+
+                this.txtStuId.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 保存
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // 验证用户输入
+            if (this.ValidateInput() == false)
+            {
+                return;
+            }
+
+            try
+            {
+                // 在临时表中添加新行，以保存最后输入的一条记录。
+                DataRow dr = dt.NewRow();
+                dr["StuId"] = this.txtStuId.Text;
+                dr["StuName"] = this.txtStuName.Text;
+                dr["Sex"] = this.cboSex.Text;
+                dr["ClassId"] = this.cboClassName.SelectedValue;
+                dr["DormId"] = this.cboDormName.SelectedValue;
+                dr["Telephone"] = this.txtTelephone.Text;
+                dr["HomeAddress"] = this.txtHomeAddress.Text;
+                dr["HomePhone"] = this.txtHomePhone.Text;
+
+                // 加入新行
+                dt.Rows.Add(dr);
+
+                int insertResult = 0;
+
+                // 遍历临时表，取出临时表中的每一行，并保存到后台数据库中。
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    //添加记录用的SQL语句
+
+                    string insertSql = String.Format("insert into Students (StuId, StuName, StuPwd, Sex, ClassId, DormId, Telephone, HomeAddress, HomePhone) " + 
+                        "values ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}') ",
+                        dt.Rows[i]["StuId"].ToString(),
+                        dt.Rows[i]["StuName"].ToString(),
+                        "888888",
+                        dt.Rows[i]["Sex"].ToString(),
+                        dt.Rows[i]["ClassId"].ToString(),
+                        dt.Rows[i]["DormId"].ToString(),
+                        dt.Rows[i]["Telephone"].ToString(),
+                        dt.Rows[i]["HomeAddress"].ToString(),
+                        dt.Rows[i]["HomePhone"].ToString());
+
+                    //执行添加操作
+                    insertResult = DBHelper.executeInsertSql(insertSql);
+                }
+
+                if (insertResult >= 1)
+                {
+                    MessageBox.Show("添加成功！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // 清空临时表
+                    dt.Rows.Clear();
+
+                    //锁定文本框
+                    this.LockedTextBox();
+
+                    this.btnInput.Enabled = true;    //启用“录入”按钮
+                    this.btnNext.Enabled = false;    //禁用“下一条”按钮
+                    this.btnSave.Enabled = false;    //禁用“保存”按钮
+                    this.btnCancel.Enabled = false;  //禁用“取消”按钮
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "操作失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // 取消
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            // 清空临时表
+            dt.Rows.Clear();
+
+            //清空文本框
+            this.ClearTextBox();
+
+            //锁定文本框
+            this.LockedTextBox();
+
+            this.btnInput.Enabled = true;    //启用“录入”按钮
+            this.btnNext.Enabled = false;    //禁用“下一条”按钮
+            this.btnSave.Enabled = false;    //禁用“保存”按钮
+            this.btnCancel.Enabled = false;  //禁用“取消”按钮
+        }
+    }
+}
